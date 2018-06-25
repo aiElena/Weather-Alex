6f72e2566e05811e9aaa978e48cb465a44199086
aiElena <ai.alexandrova@ukr.net>
2018-06-25 07:55:43 +0300

    index

--- index.php
+++ index.php
@@ -8,38 +8,36 @@
 	</head>
 	<body>
 		<h1>Список</h1>
-		<select id="selecttheme">
-			<option value="706483">Харьков</option>
-			<option value="703448">Киев</option>
-			<option value="688533">Ялта</option>		 
-		</select>
-		<hr>
-
-		<hr>
+		<section class="city">
+			<form action="" method="GET">
+				<select  name="city">
+					<option value="1">Выбрать</option>
+					<option value="706483">Харьков</option>
+					<option value="703448">Киев</option>
+					<option value="688533">Ялта</option>
+				</select>
+				<input type="submit">
+			</form>
+		</section>
 <?php
-
-	
-		
 $url = "http://api.openweathermap.org/data/2.5/weather?id=706483&lang=en&units=metric&appid=853437dd97c1de13d607d8ea9a4cbae4";
 $url1 = "http://api.openweathermap.org/data/2.5/weather?id=703448&lang=en&units=metric&appid=853437dd97c1de13d607d8ea9a4cbae4";
 $url2 = "http://api.openweathermap.org/data/2.5/weather?id=688533&lang=en&units=metric&appid=853437dd97c1de13d607d8ea9a4cbae4";
 $contents = file_get_contents($url);
 $contents1 = file_get_contents($url1);
 $contents2 = file_get_contents($url2);
-
-$url = "http://api.openweathermap.org/data/2.5/weather?id=706483&lang=en&units=metric&appid=853437dd97c1de13d607d8ea9a4cbae4";
-$url1 = "http://api.openweathermap.org/data/2.5/weather?id=703448&lang=en&units=metric&appid=853437dd97c1de13d607d8ea9a4cbae4";
-$url2 = "http://api.openweathermap.org/data/2.5/weather?id=688533&lang=en&units=metric&appid=853437dd97c1de13d607d8ea9a4cbae4";
-$contents = file_get_contents($url);
-$contents1 = file_get_contents($url1);
-$contents2 = file_get_contents($url2);
-echo ($contents);
-echo '<br>';
-echo '<br>';
-echo ($contents1);
-echo '<br>';
-echo '<br>';
-echo ($contents2);
+	if (isset($_REQUEST['city']) and $_REQUEST['city'] == 1) {
+		echo '';
+	}
+	if (isset($_REQUEST['city']) and $_REQUEST['city'] == 706483) {
+		echo $contents;
+	}
+	if (isset($_REQUEST['city']) and $_REQUEST['city'] == 703448) {
+		echo $contents1;
+	}
+	if (isset($_REQUEST['city']) and $_REQUEST['city'] == 688533) {
+		echo $contents2;
+	}
 ?>
 
 
