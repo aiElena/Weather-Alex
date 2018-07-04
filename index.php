2bcbc51b536d43bd54295cacd4ff840edabc93d6
aiElena <ai.alexandrova@ukr.net>
2018-07-04 19:17:56 +0300

    index

--- index.php
+++ index.php
@@ -26,8 +26,7 @@ if($city!=0){
  $url = "http://api.openweathermap.org/data/2.5/weather?id=".$city."&lang=en&units=metric&appid=853437dd97c1de13d607d8ea9a4cbae4"; 
   $contents = file_get_contents($url);
   echo $contents;
-  echo '<hr>';
-  echo $city;
+
 }
 ?>
 	</body>
