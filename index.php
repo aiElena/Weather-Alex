d360b6034eada3690711a0573458a76378ed9ceb
aiElena <ai.alexandrova@ukr.net>
2018-07-16 21:19:10 +0300

    index function

--- index.php
+++ index.php
@@ -23,40 +23,41 @@
 		<div id="map_canvas">
 			
 		</div>
-
-
 <?php		
 $city = $_REQUEST['city'];
+function coord($city){
+  $url = "http://api.openweathermap.org/data/2.5/weather?id=".$city."&lang=en&units=metric&appid=853437dd97c1de13d607d8ea9a4cbae4";
+  $contents = file_get_contents($url);
 
+  $clima=json_decode($contents);
+  return [
+    'lat' => $clima->coord->lat,
+	'lon' => $clima->coord->lon,
+	'name' => $clima->name,
+	'date' => date("F j, Y, g:i a"),
+	'temp' => $clima->temp_max,
+	
+  ];
+}
 
 if($city!=0){ 
- $url = "http://api.openweathermap.org/data/2.5/weather?id=".$city."&lang=en&units=metric&appid=853437dd97c1de13d607d8ea9a4cbae4";
 
-  $contents = file_get_contents($url);
-  $clima=json_decode($contents);
-  echo '<div class="info">'.$contents.'</div>';
- echo '<hr>'; 
+  $clima=coord($city);
+  
   ?>
- 
 		<div class="weather">
-			<div class="weather_city"><?php echo $clima->name;?></div>
-			<div class="lat"><?php echo $clima->coord->lat;?></div>
-			<div class="lon"><?php echo $clima->coord->lon;?></div>
-			<div class="weather_date"><?php echo date("F j, Y, g:i a") ?></div>
-			<div class="temp_max"><?php echo $clima->temp_max;?></div>
-			<div class="icon"><?php echo $clima->weather[0]->icon.".png";?></div>
+			<div class="weather_city"><?php echo $clima['name'];?></div>
+			<div class="lat"><?php echo $clima['lat'];?></div>
+			<div class="lon"><?php echo $clima['lon'];?></div>
+			<div class="weather_date"><?php echo $clima['date']; ?></div>
+			<div class="temp_max"><?php echo $clima['temp'];?></div>
+			
 		</div>
-
 		<div id="map_canvas">
-			<img src="https://www.mapquestapi.com/staticmap/v5/map?key=mAuUGJiI4qIdyLowNSA8ltquKNzGXFok&center=<?php echo $clima->coord->lat;?>,<?php echo $clima->coord->lon;?>&size=@2x" style="width: 400px; height: 400px;"/>	
+			<img src="https://www.mapquestapi.com/staticmap/v5/map?key=mAuUGJiI4qIdyLowNSA8ltquKNzGXFok&center=<?php echo $clima['lat'];?>,<?php echo $clima['lon'];?>&size=@2x" style="width: 400px; height: 400px;"/>	
 		</div>
-
 <?php }
-echo '<hr>';
-
  ?>
-
-
 
 	</body>
 </html>
